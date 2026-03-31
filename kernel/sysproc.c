060ed0d457bd464b525984ca9a10354d6b37b876
Visindu Biyanwila <vbiyanwi@gmail.com>
2026-03-31 13:54:50 -0400

    Added getidlestats system call wrapper

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,6 +6,7 @@
 #include "spinlock.h"
 #include "proc.h"
 #include "vm.h"
+#include "eco.h"
 
 uint64
 sys_exit(void)
@@ -133,6 +134,28 @@ sys_getschedmode(void)
 {
   // Return the current global scheduling mode.
   return getschedmode();
+}
+
+
+uint64
+sys_getidlestats(void)
+{
+  uint64 uaddr;
+  struct idlestats stats;
+
+  // get the user-space address passed into the syscall
+  if(argaddr(0, &uaddr) < 0)
+    return -1;
+
+  // fill the stats struct from kernel idle data
+  get_idle_stats(&stats.idle_entries, &stats.total_idle_ticks,
+                 &stats.currently_idle_cpus);
+
+  // copy the struct back to user space
+  if(copyout(myproc()->pagetable, uaddr, (char *)&stats, sizeof(stats)) < 0)
+    return -1;
+
+  return 0;
 }
 
 // Adding the syscall handler wrapper (step 3)
